5dac842ab98d63d7501a6dd1d856c3b32a8cfe75
atishaybaid <atishaybaid@gmail.com>
2020-05-03 13:52:44 +0530

    added mongodb atlas connnection

--- dist/config/dev.js
+++ dist/config/dev.js
@@ -6,7 +6,8 @@ Object.defineProperty(exports, "__esModule", {
 exports.default = void 0;
 const config = {
   "port": "3000",
-  "dbURL": "mongodb://127.0.0.1:27017/godowns",
+  //"dbURL": "mongodb://127.0.0.1:27017/godowns",
+  "dbURL": "mongodb+srv://atishaybaid:oLOBp056LM3CN2TN@review-cluster-h4hww.mongodb.net/godown?retryWrites=true&w=majority",
   "jwkKey": "reviewsBackendServer"
 };
 var _default = config;
--- src/config/dev.js
+++ src/config/dev.js
@@ -1,6 +1,7 @@
 const config = {
     "port": "3000",
-    "dbURL": "mongodb://127.0.0.1:27017/godowns",
+    //"dbURL": "mongodb://127.0.0.1:27017/godowns",
+    "dbURL": "mongodb+srv://atishaybaid:oLOBp056LM3CN2TN@review-cluster-h4hww.mongodb.net/godown?retryWrites=true&w=majority",
     "jwkKey": "reviewsBackendServer"
 
 }
